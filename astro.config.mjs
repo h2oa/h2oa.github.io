67e74f638040f442983a0e5f2e14d4ae4f73752c
h2oa <le.ngoc.hoa.hust@gmail.com>
2025-01-24 20:40:28 +0700

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://h2oa.github.io/",
+  site: "https://h2oa.github.io",
   base: "/",
   trailingSlash: "always",
   integrations: [
